598c555e3f5ab65886c6b790facf0ec40047e771
Nick Pullen <nickpullen@nicks-macbook-pro.local>
2018-10-03 12:00:38 -0700

    Initial Commit

---
+++ Chapter 3.1 Activity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter 3.1 Activity
+//
+//  Created by Nick  Pullen on 10/3/18.
+//  Copyright © 2018 Nick  Pullen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
